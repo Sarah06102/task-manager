251227db6f1dc1d16bad28d2d63816774ab4d252
Sarah06102 <sarah.hra06@gmail.com>
2025-10-02 16:27:39 -0400

    updated backend url in vite config file

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
-console.log("API_BASE:", API_BASE);
+const API_BASE = process.env.REACT_APP_API_URL
 
 const Login = () => {
     // Initialize variables 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5050',
+        target: 'https://task-manager-xvhd.onrender.com',
         changeOrigin: true,
       },
     },
