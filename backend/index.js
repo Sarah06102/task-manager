254b7c33c6d66ab5ccfb8f02f5efde153df82977
Sarah06102 <sarah.hra06@gmail.com>
2025-10-02 16:25:32 -0400

    Updated origin for cors in backend

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: [ 
-        'http://localhost:5174', 
-        'https://task-manager-xi-five-71.vercel.app/'
-    ],
+    origin:'https://task-manager-xi-five-71.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: false,
     allowedHeaders: ['Content-Type', 'Authorization']
